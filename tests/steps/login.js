a4047d1e984c1d92bf0c03cb6f2157481c6ba886
ankita02051987 <ankita0205@hotmail.co.uk>
2025-04-20 18:20:37 +0100

    1stCommit for cucumber using steps autogeneration

---
+++ tests/steps/login.js
@@ -0,0 +1,33 @@
+
+       
+         Given('I visit the login page', async function () {
+           // Write code here that turns the phrase above into concrete actions   
+           return 'pending';
+         });
+       
+  
+       
+         When('the user enters a valid username and password', async function () {
+           // Write code here that turns the phrase above into concrete actions   
+           return 'pending';
+         });
+
+
+
+         When('clicks the login button', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+         Then('the user is navigated to the product page', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+ 
+
+         Then('sees all products listed', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
