d10c9a37aba194fa0e72b0898f340bfe4836c1aa
ankita02051987 <ankita0205@hotmail.co.uk>
2025-04-20 18:48:02 +0100

    1st

--- tests/steps/login.js
+++ tests/steps/login.js
@@ -1,33 +1,37 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { chromium } = require('playwright');
+
+let browser, page;
 
        
          Given('I visit the login page', async function () {
-           // Write code here that turns the phrase above into concrete actions   
-           return 'pending';
+           browser = await chromium.launch({headless : false}) 
+           const context = await browser.newContext();
+           page = await context.newPage();
+           await page.goto('http://localhost:5173'); 
          });
        
   
        
-         When('the user enters a valid username and password', async function () {
-           // Write code here that turns the phrase above into concrete actions   
-           return 'pending';
-         });
-
-
-
-         When('clicks the login button', async function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-
-         Then('the user is navigated to the product page', async function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
- 
-
-         Then('sees all products listed', async function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+         When('the user enters a valid username and password',{ timeout: 10000 }, async function () {
+            await page.fill('input[placeholder="Enter email"]', 'ank@gmail.com'); // Adjust the input selector if needed
+            await page.fill('input[placeholder="Password"]', 'secret_sauce'); // Placeholder password
+          });
+          
+          When('clicks the login button',{ timeout: 10000 }, async function () {
+            await page.click('button[type="submit"]'); // Adjust selector if needed
+          });
+          
+          Then('the user is navigated to the product page', { timeout: 10000 },async function () {
+            await page.waitForSelector('text=Products'); // Or page.waitForURL('**/products')
+          });
+          
+          Then('sees all products listed', { timeout: 10000 }, async function () {
+            const products = await page.$$('.product-item');
+            if (products.length < 6) {
+              throw new Error(`Expected 6 products, but found ${products.length}`);
+            }
+          
+            await browser.close();
+          });
+          
